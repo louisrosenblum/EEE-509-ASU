16cc5454dcfe6c02f16a8267779b1b496be5a369
louisrosenblum <42879481+louisrosenblum@users.noreply.github.com>
2020-06-11 21:06:07 -0700

    Started project2.m

--- Project 2/project2.m
+++ Project 2/project2.m
@@ -2,3 +2,19 @@
 % Project 2
 % EEE 509 - ASU
 % 06/10/2020
+
+%% Initialization
+
+clear all
+close all
+
+cd 'C:\Users\Louis\Desktop\DSP\Project 2'
+%% Read audio from file
+
+[mic1, Fs1] = audioread('mic1_2019.wav');
+[mic2, Fs2] = audioread('mic2_2019.wav');
+
+length1 = length(mic1);
+length2 = length(mic2);
+
+%% 
